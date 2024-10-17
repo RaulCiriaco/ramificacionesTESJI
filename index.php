815714510f0775e123da29ccd6d0fcec55d7bfe7
RaulCiriaco <ciriacoral1567@gmail.com>
2024-10-16 21:32:00 -0600

    holaMundoPHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+    <h1>Soy un encabezado de HTML</h1>
+    // lo que va dentro de aqui es lenguaje php
+    <?php
+    # Script PHP
+    // Tambien soy un comentario de una linea
+    /* Soy un comentario
+        de multiples lineas */
+        echo "<h2>Hola Mundo desde PHP</h2>";
+    ?>    
+</body>
+</html>
